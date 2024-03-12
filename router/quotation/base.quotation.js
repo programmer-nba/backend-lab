c6a5f4267b8264b63d1b8c63e43421657767a142
นนท์ <nonin31313@gmail.com>
2024-03-12 10:54:34 +0700

    Update base.quotation.js

--- router/quotation/base.quotation.js
+++ router/quotation/base.quotation.js
@@ -3,10 +3,10 @@ const quotation = require("../../controllers/sale/base.quotation.controller");
 const auth = require("../../auth/auth");
 
 router.post("/create", auth.all, quotation.BaseQuotation);
-router.get("/GetAllQuotation", auth.all, quotation.GetAllQuotation)
-router.get("/GetBaseQuotationBy/:id", auth.all, quotation.GetBaseQuotationById)
-router.put("/edit/:id", auth.all, quotation.edit)
-router.delete("/delete/:id", auth.all, quotation.deleteQtByid)
-router.delete("/deleteAll", auth.all, quotation.deleteAllQt)
+router.get("/GetAllQuotation", auth.all, quotation.GetAllQuotation);
+router.get("/GetBaseQuotationBy/:id", auth.all, quotation.GetBaseQuotationById);
+router.put("/edit/:id", auth.all, quotation.edit);
+router.delete("/delete/:id", auth.all, quotation.deleteQtByid);
+router.delete("/deleteAll", auth.all, quotation.deleteAllQt);
 
 module.exports = router;
