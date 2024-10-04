1c09d0864adaea7c36e8e0f6e313f4882b99a898
KieKoong <kiekoong.dev@gmail.com>
2024-10-04 15:02:10 +0700

    xxzz

--- router/admin/index.js
+++ router/admin/index.js
@@ -3,6 +3,7 @@ const admin = require("../../controllers/Admin/admin.cotrollers")
 const auth = require("../../auth/auth")
 
 //เพิ่มลบเเก้ไขข้อมูลเเอดมิน
+router.post("/create",admin.create)
 router.put("/EditAdmin/:id",auth.admin,admin.EditAdmin)
 router.delete("/deleteAdmins/:id", auth.admin, admin.deleteAdmins);
 router.get("/getAdminAll", auth.all, admin.getAdminAll);
